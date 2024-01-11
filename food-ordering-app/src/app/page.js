58d072853862ff8b9e42731ff7481f05ed14e27d
Matt-mdj <mattmdj77@gmail.com>
2024-01-11 15:09:45 -0800

    Working on Auth

--- food-ordering-app/src/app/page.js
+++ food-ordering-app/src/app/page.js
@@ -2,6 +2,7 @@ import Image from 'next/image'
 import Header from '@/components/Layout/Header'
 import Hero from '@/components/Layout/Hero'
 import HomeMenu from '@/components/Layout/HomeMenu'
+import SectionHeaders from '@/components/Layout/SectionHeaders'
 
 
 export default function Home() {
@@ -10,6 +11,38 @@ export default function Home() {
       <Header />
       <Hero />
       <HomeMenu/>
+      <section className='text-center my-16'>
+        <SectionHeaders subHeader={'Our Story'} mainHeader={'About Us'}/>
+        <div className='text-gray-500 max-w-md mx-auto mt-4 flex flex-col gap-4'>
+          <p >
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+            Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+            Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris 
+            nisi ut aliquip ex ea commodo consequat.
+          </p>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+            Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+            Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris 
+            nisi ut aliquip ex ea commodo consequat.
+          </p>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+            Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+          </p>
+        </div>
+      </section>
+      <section className="text-center my-8">
+        <SectionHeaders subHeader={'Don\'t Hesitate'} mainHeader={'Contact Us'}/>
+        <div className='mt-8'>
+          <a className="text-4xl underline text-grey-500" href="tel:+46590403030">
+            +46 590 403 030
+          </a>
+        </div>
+      </section>
+      <footer className="border-t p-8 text-center text-grey-500 mt-16">
+        &copy; 2023 All Rights Reserved
+      </footer>
     </>
   )
 }
--- food-ordering-app/src/components/Layout/HomeMenu.js
+++ food-ordering-app/src/components/Layout/HomeMenu.js
@@ -1,10 +1,29 @@
+import Image from "next/image";
+import MenuItem from "../menu/menuItem";
+import SectionHeaders from "./SectionHeaders";
+
 export default function HomeMenu(){
     return (
-        <div>
-            <h3>
-                CheckOut
-            </h3>
-            <h2>Menu</h2>
-        </div>
+        <section>
+            <div className="absolute  left-0 right-0 w-full justify-start ">
+                <div className="absolute left-0 -top-[70px] text-left -z-10">
+                    <Image src={'/sallad1.png'} width={109} height={189} alt={'salad'}/>
+                </div>
+                <div className="absolute -top-[100px] right-0 -z-10">
+                    <Image  src={'/sallad2.png'} width={107} height={195} alt={'salad'}/>
+                </div>
+            </div>
+            <div className="text-center mb-4"> 
+                <SectionHeaders subHeader={'check out'} mainHeader={'Menu'}/>
+            </div>
+            <div className="grid grid-cols-3 gap-4">
+                <MenuItem/>
+                <MenuItem/>
+                <MenuItem/>
+                <MenuItem/>
+                <MenuItem/>
+                <MenuItem/>
+            </div>
+        </section>
     );
 }
