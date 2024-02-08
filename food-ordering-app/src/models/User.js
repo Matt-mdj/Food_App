79e62fbee5b17e3deeb86f7631856aa64389336a
Matt-mdj <mattmdj77@gmail.com>
2024-02-07 18:00:36 -0800

    Update User.js

--- food-ordering-app/src/models/User.js
+++ food-ordering-app/src/models/User.js
@@ -3,15 +3,7 @@ import bcrypt from 'bcrypt';
 
 const UserSchema = new Schema({
     email:{type:String, required: true, unique: true},
-    password: {
-        type: String, 
-        required: true, 
-        validate: pass => {
-            if(!pass?.length ||pass.length < 5){
-                new Error('password must be at least 5 characters');
-            }
-        },
-    },
+    password: {type: String},
 }, {timestamps: true});
 
 UserSchema.post('validate', function(user){
